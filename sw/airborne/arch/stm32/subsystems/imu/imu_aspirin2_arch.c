3944552c75a662e0de712f3a80c9ba057b4837f1
Piotr Esden-Tempski <piotr@esden.net>
2013-02-22 11:41:01 -0800

    Made baro CS pull up. Should work with aspirin 2.1 as well as 2.2.

--- sw/airborne/arch/stm32/subsystems/imu/imu_aspirin2_arch.c
+++ sw/airborne/arch/stm32/subsystems/imu/imu_aspirin2_arch.c
@@ -38,6 +38,19 @@ void imu_aspirin2_arch_int_disable(void) {
 }
 
 void imu_aspirin2_arch_init(void) {
+
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Set baro CS (PC.13) as input with pullup to prevent floating input
+   * Aspirin 2.1 has this pin connected to ground
+   * Aspirin 2.2 has the ms5611 baro CS on this line
+   */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+	
 /*
   GPIO_InitTypeDef GPIO_InitStructure;
   EXTI_InitTypeDef EXTI_InitStructure;
