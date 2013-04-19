174e69237423d9ad300f40e9a5fa0f8551b7c278
Piotr Esden-Tempski <piotr@esden.net>
2013-04-18 17:17:05 -0700

    Made it so that the baro gpio pin is actually being configured.
    Note: arch init function for aspirin2 is not being called...

--- sw/airborne/arch/stm32/subsystems/imu/imu_aspirin2_arch.c
+++ sw/airborne/arch/stm32/subsystems/imu/imu_aspirin2_arch.c
@@ -41,16 +41,19 @@ void imu_aspirin2_arch_init(void) {
 
   GPIO_InitTypeDef GPIO_InitStructure;
 
-  /* Set baro CS (PC.13) as input with pullup to prevent floating input
-   * Aspirin 2.1 has this pin connected to ground
-   * Aspirin 2.2 has the ms5611 baro CS on this line
+  /**************************************
+   * THIS FUNCTION IS NOT BEING CALLED!!!
+   **************************************
+   * As this function has everything
+   * commented out, CDW commented out the
+   * call to it from the arch independent
+   * aspirin driver. So be aware that
+   * if you add code here it will not get
+   * invoked!!!
+   *
+   * Sincirely yours Esden...
    */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-	
+
 /*
   GPIO_InitTypeDef GPIO_InitStructure;
   EXTI_InitTypeDef EXTI_InitStructure;
